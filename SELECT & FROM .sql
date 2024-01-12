bbc760f75969c6b8e3c3e161406752c21bc953c0
Glen <155974295+Glen-Ochieng@users.noreply.github.com>
2024-01-12 12:49:26 +0300

    Update SELECT & FROM .sql

--- SELECT & FROM .sql
+++ SELECT & FROM .sql
@@ -8,4 +8,15 @@ and country="USA";
 select contactFirstName,contactLastName
 from customers 
 where country="Norway"
-or country="Sweden"
+or country="Sweden";
+
+select *
+from customers
+where country="USA"
+and contactLastName = 'Brown'
+or country="UK"
+and contactLastName = 'Brown';
+
+select email
+from employees
+where jobTitle="Sales Rep"
