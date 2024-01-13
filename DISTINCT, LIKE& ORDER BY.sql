06552c03084390a62218c9f06d8b0488548d7a2a
Glen <155974295+Glen-Ochieng@users.noreply.github.com>
2024-01-13 12:38:04 +0300

    Add files via upload

---
+++ DISTINCT, LIKE& ORDER BY.sql
@@ -0,0 +1,24 @@
+select *
+from customers 
+where city like '%New%' ;
+
+select *
+from employees
+where lastName like '%M%';
+
+select*
+from customers
+where upper(city) like '%NEW%'
+order by contactFirstName;
+
+select*
+from customers
+where upper(city) like '%NEW%'
+order by  contactFirstName desc;
+
+#to get both first name and last name in descending order use the following code#
+
+select*
+from customers
+where upper(city) like '%NEW%'
+order by  contactFirstName desc, contactLastName desc;
