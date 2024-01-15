6ff7a16f39c5cdc7bb2cf572f1690ff8d2057527
Glen <155974295+Glen-Ochieng@users.noreply.github.com>
2024-01-15 18:26:41 +0300

    Update JOINS.sql

--- JOINS.sql
+++ JOINS.sql
@@ -3,7 +3,7 @@ from orders t1
 inner join customers t2 
 on t1.customerNumber= t2.customerNumber
 
-*However, the columname customerNumber will appear twice in the joined table*
+/*However, the columname customerNumber will appear twice in the joined table*/
 
 select customerName,contactLastName,contactFirstName
 from employees t1
