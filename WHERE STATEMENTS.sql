8e92f3af493d31c3ae5fd222db6f067bb2abb921
Glen <155974295+Glen-Ochieng@users.noreply.github.com>
2025-11-09 17:28:42 +0300

    Update WHERE STATEMENTS.sql

--- WHERE STATEMENTS.sql
+++ WHERE STATEMENTS.sql
@@ -12,3 +12,17 @@ select * from prices
 
 select * from prices
 WHERE price between 50 and 60;
+
+*Eg*
+select * from products 
+  WHERE categories = "Gadget" or categories = "Gizmo";
+
+*OR*
+
+select * from products 
+  WHERE categories in ("Gadgets","Gizmo");
+
+/*For multiple conditions you can place each condition in brackets to seperate them.*/
+
+select * from products 
+  WHERE (categories in ('Gadget', 'Gizmo') ) and (price >50);
