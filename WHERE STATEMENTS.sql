178e397929c7979be63a6ceee48a8a50fda496b8
Glen <155974295+Glen-Ochieng@users.noreply.github.com>
2025-11-09 13:04:46 +0300

    Create WHERE STATEMENTS.sql

---
+++ WHERE STATEMENTS.sql
@@ -0,0 +1,14 @@
+/* Where statements are used to filter tables*/
+
+*Syntax*
+/* select * from table_name WHERE */
+
+*Eg*
+
+select * from prices 
+  WHERE price >= 50 and price <= 60;
+
+*which can also be written as*
+
+select * from prices
+WHERE price is between 50 and 60;
