089ef00593eedac0b612d42d364040189db1e1d6
Glen <155974295+Glen-Ochieng@users.noreply.github.com>
2025-11-09 17:51:50 +0300

    Update VIEWS.sql

--- VIEWS.sql
+++ VIEWS.sql
@@ -1,8 +1,13 @@
-/* Views are saves of the sql query. They are useful if you wish to save the code for future repetitive tasks.*/
+/* Views are saves of the sql query. They are useful if you wish to save the code for future repetitive tasks. They are also useful for sub-quering.*/
 
 /*Syntax
 CREATE VIEW view_name AS
 SELECT column1, column2, ...
 FROM table_name
 WHERE condition;
+*/
+
+/*IMPORTANT TO NOTE: 
+ORDER BY DOES NOT WORK WITH VIEWS.
+AS SUCH IT IS IMPORTANT TO SUBQUERY THE VIEW IF YOU WISH TO ODRER BY 
 */
