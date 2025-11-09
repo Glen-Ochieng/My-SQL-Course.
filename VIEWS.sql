f5b10e927913a3307d8e028d44deb0c30e894346
Glen <155974295+Glen-Ochieng@users.noreply.github.com>
2025-11-09 17:46:22 +0300

    Create VIEWS.sql

---
+++ VIEWS.sql
@@ -0,0 +1,8 @@
+/* Views are saves of the sql query. They are useful if you wish to save the code for future repetitive tasks.*/
+
+/*Syntax
+CREATE VIEW view_name AS
+SELECT column1, column2, ...
+FROM table_name
+WHERE condition;
+*/
